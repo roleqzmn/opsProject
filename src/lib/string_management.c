d2de5dfc9ad3307d5d637d99aeed75de95c7c092
roleq_zmn <maciej.majewski06@gmail.com>
2025-12-23 13:22:39 +0100

    moved two functions from main to string_management

--- src/lib/string_management.c
+++ src/lib/string_management.c
@@ -1,4 +1,6 @@
+#define _GNU_SOURCE
 #include <errno.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -37,4 +39,89 @@ char *replace_prefix(const char *path, const char *old_prefix, const char *new_p
     strcpy(result, new_prefix);
     strcat(result, path + old_len);
     return result;
+}
+
+char *expand_tilde(const char *path)
+{
+    if (!path || path[0] != '~')
+    {
+        return strdup(path);
+    }
+
+    const char *home = getenv("HOME");
+    if (!home)
+    {
+        fprintf(stderr, "Brak zmiennej HOME!\n");
+        return NULL;
+    }
+
+    size_t home_len = strlen(home);
+    size_t path_len = strlen(path);
+
+    char *result = malloc(home_len + path_len);
+    if (!result)
+        return NULL;
+
+    strcpy(result, home);
+    strcat(result, path + 1);
+
+    return result;
+}
+
+int parse_command_line(char *line, char *command, char **args)
+{
+    size_t pos = 0;
+    size_t line_len = strlen(line);
+
+    while (pos < line_len && line[pos] == ' ')
+        pos++;
+    size_t cmd_start = pos;
+    while (pos < line_len && line[pos] != ' ')
+        pos++;
+
+    strncpy(command, line + cmd_start, pos - cmd_start);
+    command[pos - cmd_start] = '\0';
+
+    if (strlen(command) == 0)
+    {
+        return 0;
+    }
+
+    int arg_count = 0;
+    while (pos < line_len && arg_count < MAX_ARGS)
+    {
+        while (pos < line_len && line[pos] == ' ')
+            pos++;
+        if (pos >= line_len)
+            break;
+
+        char arg_buf[PATH_MAX];
+        int arg_idx = 0;
+
+        if (line[pos] == '"')
+        {
+            pos++;
+            while (pos < line_len && line[pos] != '"')
+            {
+                arg_buf[arg_idx++] = line[pos];
+                pos++;
+            }
+            if (pos < line_len)
+                pos++;
+        }
+        else
+        {
+            while (pos < line_len && line[pos] != ' ')
+            {
+                arg_buf[arg_idx++] = line[pos];
+                pos++;
+            }
+        }
+        arg_buf[arg_idx] = '\0';
+        args[arg_count] = strdup(arg_buf);
+
+        arg_count++;
+    }
+
+    return arg_count;
 }
--- src/lib/string_management.h
+++ src/lib/string_management.h
@@ -6,10 +6,18 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_LINE 4096
+#define MAX_ARGS 100
+
 char *file_path(const char *dir,
                 const char *file_name);  // constructs a full file path from
                                          // directory and file/directory name
 
 char *replace_prefix(const char *path, const char *old_prefix,
                      const char *new_prefix);  // replaces old_prefix with new_prefix in path
-#endif                                         // STRING_MANAGEMENT_H
+
+char *expand_tilde(const char *path);  // expands ~ to the user's home directory
+
+int parse_command_line(char *line, char *command,
+                       char **args);  // parses a command line into command and arguments
+#endif                                // STRING_MANAGEMENT_H
